e1c5728882fbb85a0db74f012ca539d35b2e02ed
Eric Chin <ericychin94@gmail.com>
2022-11-06 01:04:23 -0800

    Added morgan middleware to show GET request status in console

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const cors = require('cors');
+const morgan = require('morgan');
 
-app.use(express.json());
+morgan.token("data", (req) => {
+  return JSON.stringify(req.body);
+});
+
 app.use(cors());
+app.use(express.json());
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"));
 
 const apiKey = 'a2813bf7a97824733c620ca3696826b4';
 
