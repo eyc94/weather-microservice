5446ab72751379b5b2ac7ce97eb00f7e077ff9f9
Eric Chin <ericychin94@gmail.com>
2022-11-06 01:58:29 -0700

    Allow CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 const apiKey = 'a2813bf7a97824733c620ca3696826b4';
 
